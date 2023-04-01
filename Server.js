2b81eec082aa506ae13c76f41e017d5f863df65f
Gupta1403 <48518912+Gupta1403@users.noreply.github.com>
2023-04-01 15:51:28 +0530

    File upload BE+FE code

---
+++ Server.js
@@ -0,0 +1,46 @@
+const express = require('express'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    multer = require('multer'),
+    uuidv4 = require('uuid/v4');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+const DIR = './uploads/';
+app.use('/images', express.static(DIR));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, DIR);
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        cb(null, uuidv4() + '-' + fileName)
+    }
+});
+var upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+        }
+    }
+});
+
+app.post('/user-profile', upload.single('profileImg'), (req, res, next) => {
+    console.log(req.body.formData)
+    const user = {
+        name: req.body.name,
+        profileImg: req.file.filename
+    };
+    
+    res.send({'message': 'Date recieved and pic uploaded successfully', user})
+})
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log('Connected to port ' + port))
